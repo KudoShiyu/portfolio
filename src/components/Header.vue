a9b0013fd10c1d027630ec8dd192dd15f51b6d1b
KudoShiyu <kudo.shiyu@gmail.com>
2022-01-02 01:40:06 +0900

    added global style

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="header">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+export default class Header extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#header {
+  padding: 30px;
+a {
+    font-weight: bold;
+    color: $c_gray;
+    &.router-link-exact-active {
+      color: $c_green;
+    }
+  }
+}
+</style>
